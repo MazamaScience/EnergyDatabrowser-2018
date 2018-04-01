83b4f32ac72df86d3e1b4e9b6193c0ffb7c2f2f2
Kushal <kns94>
2018-04-01 15:12:15 -0700

    Created a basic line chart for all resources

--- UI/js/scatter_plot.js
+++ UI/js/scatter_plot.js
@@ -10,20 +10,33 @@ function ready(error, loaded_json){
 }
 
 
-
 function plotPatternData(unit, country, resource_pattern){
     /*Aim is to get timeseries data for a given country, unit for all resources for all years*/
 
-    var resource_data = {};
+    var resource_data = [];
 
     for (var resource in merged_json[unit]){
-        resource_data[resource] = [];
         country_resource = merged_json[unit][resource][country];
-        //Parsing all years
+        //Parsing value from all years
+        years = d3.keys(country_resource);
+        values = [];
         for(var year in country_resource){
-            pattern_value = country_resource[year][resource_pattern];
-            resource_data[resource].push({"Date": year, "Value": pattern_value})
+            values.push(country_resource[year][resource_pattern]);
         }
+
+        var trace = {
+          type: "scatter",
+          mode: "lines",
+          name: resource,
+          x: years,
+          y: values
+        }
+
+        resource_data.push(trace);
     }
-    console.log(resource_data);
+    
+    var layout = {
+        title: 'India energy resource consumption for all years', 
+    };
+    Plotly.newPlot('consumptionChart', resource_data, layout);
 }
