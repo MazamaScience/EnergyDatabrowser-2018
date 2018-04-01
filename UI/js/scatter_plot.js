f34db81c20808e6ca6044b2b403bbb674f6bf5df
Kushal <kns94>
2018-04-01 14:29:29 -0700

    modified json structure and trying to parse the json

---
+++ UI/js/scatter_plot.js
@@ -0,0 +1,22 @@
+d3.queue()
+    .defer(d3.json, "../Data/BPStatReview/bp_stat_review_2017_combined.json")
+    .await(ready);
+
+var merged_json;
+
+function ready(error, loaded_json){
+    merged_json = loaded_json;
+}
+
+plotPatternData('mtoe', 'IN', 'consumption')
+
+function plotPatternData(unit, country, resource_pattern){
+    /*Aim is to get timeseries data for a given country, unit for all resources for all years*/
+
+    var resource_data = {};
+
+    for (var resource in merged_json[unit]){
+        resource_data[resource] = []
+    }
+
+}
