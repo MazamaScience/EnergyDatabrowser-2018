0a074fbbea24a588bab5d52466718ac1859aae79
Kushal <kns94>
2018-04-01 14:41:22 -0700

    Parsing consumption values from json done

--- UI/js/scatter_plot.js
+++ UI/js/scatter_plot.js
@@ -6,9 +6,10 @@ var merged_json;
 
 function ready(error, loaded_json){
     merged_json = loaded_json;
+    plotPatternData('mtoe', 'IN', 'consumption')
 }
 
-plotPatternData('mtoe', 'IN', 'consumption')
+
 
 function plotPatternData(unit, country, resource_pattern){
     /*Aim is to get timeseries data for a given country, unit for all resources for all years*/
@@ -16,7 +17,13 @@ function plotPatternData(unit, country, resource_pattern){
     var resource_data = {};
 
     for (var resource in merged_json[unit]){
-        resource_data[resource] = []
+        resource_data[resource] = [];
+        country_resource = merged_json[unit][resource][country];
+        //Parsing all years
+        for(var year in country_resource){
+            pattern_value = country_resource[year][resource_pattern];
+            resource_data[resource].push({"Date": year, "Value": pattern_value})
+        }
     }
-
+    console.log(resource_data);
 }
