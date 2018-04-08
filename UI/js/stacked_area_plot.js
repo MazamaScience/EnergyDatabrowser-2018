acd0834e4dfb7a59d9884578ca6786b9440065ee
jay-chauhan <jayc.9211@gmail.com>
2018-04-07 18:23:33 -0700

    added update on dropdown for stack and scatter chart

--- UI/js/stacked_area_plot.js
+++ UI/js/stacked_area_plot.js
@@ -4,14 +4,22 @@ d3.queue()
 
 var merged_json;
 
-function ready(error, loaded_json)
-{
-	merged_json = loaded_json;
-	plotStackChart("mtoe","IN","consumption");
-
+function ready(error, loaded_json){
+    merged_json = loaded_json;
+    var cid_sp = document.getElementById("countryID");
+  var countryID_sp = cid_sp.options[cid_sp.selectedIndex].value;
+  var country_sp = cid_sp.options[cid_sp.selectedIndex].text;
+  plotStackChart('mtoe', countryID_sp, country_sp, 'consumption');
 }
 
-function plotStackChart(unit, country, resource_pattern)
+function updateChart(){
+  var cid_sp = document.getElementById("countryID");
+  var countryID_sp = cid_sp.options[cid_sp.selectedIndex].value;
+  var country_sp = cid_sp.options[cid_sp.selectedIndex].text;
+  updateStackChart('mtoe', countryID_sp, country_sp, 'consumption');
+}
+
+function plotStackChart(unit, country, country_name, resource_pattern)
 {
   var stacked_resource_data = [];
   var line_resource_data = [];
@@ -71,7 +79,8 @@ function plotStackChart(unit, country, resource_pattern)
           marker: {
            color: markers[resource]['color'],
            symbol: markers[resource]['symbol']
-          }
+          },
+          hoverinfo: 'none'
         }
      
       stacked_resource_data.push(stacked_trace);
@@ -79,8 +88,68 @@ function plotStackChart(unit, country, resource_pattern)
   }
   
   var layout = {
-      title: 'India energy resource consumption for all years', 
+      title: country_name+' energy resource consumption for all years', 
   };
   Plotly.newPlot('stackChart', stacked_resource_data, layout);
   Plotly.newPlot('consumptionChart', line_resource_data, layout);
+}
+
+
+function updateStackChart(unit, country, country_name, resource_pattern)
+{
+  var stacked_resource_data = [];
+  var line_resource_data = [];
+  var first = true;
+  var cummulative_values = [];
+  var resources = ['nuclear', 'coal', 'oil', 'gas', 'hydro'];
+  var markers = {
+    'nuclear': {'symbol': 18, 'color': 'Gold'},
+    'coal': {'symbol': 31, 'color': 'black'},
+    'oil': {'symbol': 27, 'color': 'LightGray'},
+    'gas': {'symbol': 5, 'color': 'Aqua'},
+    'hydro': {'symbol': 28, 'color': 'blue'}
+  };
+
+  //The order of resources have to be fixed for stacked area
+  for(var i in resources){
+      resource = resources[i];
+      country_resource = merged_json[unit][resource][country];
+      //Parsing value from all years
+      years = d3.keys(country_resource);
+
+      //Initializing the cummulative value array
+      if(first == true){
+        cummulative_values =  new Array(years.length).fill(0);  
+        first = false;
+      }
+      //But we have to display original value
+      var values = new Array(years.length).fill(0);
+     
+      for(var i = 0; i < years.length; i++){
+          year = years[i];
+          current_value = country_resource[year][resource_pattern];
+          cummulative_values[i] = cummulative_values[i] + current_value;
+          values[i] = current_value;
+      }
+
+      stacked_resource_data.push(cummulative_values.slice());
+      line_resource_data.push(values);
+
+      var stacked_trace = {
+
+        y: stacked_resource_data,
+      }
+      
+      var line_trace = {
+          y: line_resource_data,
+        }
+     
+      
+  }
+  
+  var layout = {
+      title: country_name+' energy resource consumption for all years', 
+  };
+  Plotly.update('stackChart', stacked_trace, layout);
+  Plotly.update('consumptionChart', line_trace, layout);
 }
