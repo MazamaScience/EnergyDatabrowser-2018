e8de6807fffe5d120f167ea73d014e3bf840de46
Mohit Patel <patelmohitj91@gmail.com>
2018-04-15 16:06:36 -0700

    stacked area chart commit sample

--- UI/js/stacked_production_consumption.js
+++ UI/js/stacked_production_consumption.js
@@ -7,11 +7,23 @@ var years = [];
 function ready(error, loaded_json)
 {
   merged_json = loaded_json;
-  plotStackChart("mtoe","EG","oil");
+  var cid_sp = document.getElementById("countryID");
+    var countryID_sp = cid_sp.options[cid_sp.selectedIndex].value;
+    var country_sp = cid_sp.options[cid_sp.selectedIndex].text;
+  plotStackChart("mtoe",countryID_sp,country_sp,"oil");
 
 }
 
-function plotStackChart(unit, country, resource)
+function updateDiffChart()
+{
+    var cid_sp = document.getElementById("countryID");
+    var countryID_sp = cid_sp.options[cid_sp.selectedIndex].value;
+    var country_sp = cid_sp.options[cid_sp.selectedIndex].text;
+    updateStackChart("mtoe",countryID_sp,country_sp,"oil");
+
+}
+
+function plotStackChart(unit, country_id,country_text, resource)
 {
 var resource_data = [];
 var traceflag = "tozeroy";
@@ -19,7 +31,7 @@ var traceflag = "tozeroy";
     for (data in merged_json[unit][resource])
       {
         
-        country_data = merged_json[unit][resource][country];
+        country_data = merged_json[unit][resource][country_id];
         //Parsing value from all years
         years = d3.keys(country_data);
        
@@ -58,12 +70,107 @@ var traceflag = "tozeroy";
           mode: "none"
         };
 
-        resource_data = [trace1,trace2];
+        var trace3 = {
+          type: "scatter",
+          x: years,
+          y: [100,200,300,400],
+          
+          
+          name: "Export"
+          
+        };
+
+        var trace4 = {
+          type: "scatter",
+          x: years,
+          y: [-100,-200,-300,-400],
+          
+      
+          name: "Import"
+
+        };
+        resource_data = [trace1,trace2,trace3,trace4];
     }
 
 
     var layout = {
-        title: country +"YPT"+ " " + resource + ' trends for ' + years[0] + ' - ' + years[years.length - 1 ]
+        title: country_text + " " + resource + ' trends for ' + years[0] + ' - ' + years[years.length - 1 ]
+    };
+    Plotly.newPlot('stackChart_production_consumption', resource_data,);
+}
+
+function updateStackChart(unit, country_id,country_text, resource)
+{
+var resource_data = [];
+var traceflag = "tozeroy";
+
+    for (data in merged_json[unit][resource])
+      {
+        
+        country_data = merged_json[unit][resource][country_id];
+        //Parsing value from all years
+        years = d3.keys(country_data);
+       
+        production_values = [];
+        for(var year in country_data){
+            production_values.push(country_data[year]["production"]);
+        }
+        
+        consumption_values = [];
+
+        for(var year in country_data){
+            consumption_values.push(country_data[year]["consumption"]);
+        }
+        
+        /*for (data in production_values)
+          {console.log(production_values[data])} */
+
+        var trace1 = {
+          type: "scatter",
+          x: years,
+          y: production_values,
+          fill: "tonexty",
+          
+          
+          name: "Production",
+          mode: "none"
+        };
+        var trace2 = {
+          type: "scatter",
+          x: years,
+          y: consumption_values,
+          fill: "tozeroy",
+          
+          
+          name: "Consumption",
+          mode: "none"
+        };
+        
+        var trace3 = {
+          type: "scatter",
+          x: years,
+          y: [100,200,300,400],
+          
+          
+          name: "Export"
+          
+        };
+
+        var trace4 = {
+          type: "scatter",
+          x: years,
+          y: [-100,-200,-300,-400],
+          
+      
+          name: "Import"
+
+        };
+        resource_data = [trace1,trace2,trace3,trace4];
+    }
+
+
+    var layout = {
+        title: country_text + " " + resource + ' trends for ' + years[0] + ' - ' + years[years.length - 1 ]
     };
     Plotly.newPlot('stackChart_production_consumption', resource_data, layout);
 }
